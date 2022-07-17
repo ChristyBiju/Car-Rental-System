5a27b7a68e021a869f17a811c59183ab27fce3a2
Christy Biju <89544124+ChristyBiju@users.noreply.github.com>
2022-07-18 00:31:27 +0530

    Add files via upload

---
+++ MyApp/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from MyApp import views
+
+urlpatterns = [
+    path("",views.index, name = 'home'),
+    path("home",views.index, name = 'home'),
+    path("about",views.about,name = 'about'),
+    path("vehicles", views.vehicles, name= "vehicles"),
+    path("register", views.register, name="register"),
+    path("signin", views.signin, name="signin"),
+    path("signout",views.signout,name = "signout")
+    # path("contact",views.contact,name = 'contact'),
+    # path("vehicles",views.Order,name = 'vehicles'),
+    # path("bike",views.bike,name = 'bike'),
+    # path("bus",views.bus,name = 'bus'),
+    
+    # path("bill", views.bill, name="bill"),
+
+    ]
