fdca7621e0f238aca53c3d63b37d24cd8fcb8fc9
Christy Biju <89544124+ChristyBiju@users.noreply.github.com>
2022-07-21 00:02:59 +0530

    Delete MyApp directory

--- MyApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Car,Order
-
-admin.site.register(Car)
-admin.site.register(Order)
--- MyApp/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-# christy123
-# Christy@2002
-
-# Create your tests here.
